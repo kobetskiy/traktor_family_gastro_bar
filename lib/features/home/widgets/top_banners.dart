18e0f77f77895e7912c611aeb9c8d1387153cf3d
kobetskiy <kobetskiy22@gmail.com>
2024-04-05 16:07:59 +0300

    adapted top banner

--- lib/features/home/widgets/top_banners.dart
+++ lib/features/home/widgets/top_banners.dart
@@ -2,33 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:traktor_family_gastro_bar/core/ui/images.dart';
 
 class TopBanners extends StatelessWidget {
-  const TopBanners({super.key, required this.tabController});
+  const TopBanners({super.key, required this.bannerController});
 
-  final TabController? tabController;
+  final TabController? bannerController;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: AspectRatio(
-        aspectRatio: 2 / 1,
-        child: PageView.builder(
-          onPageChanged: (index) => tabController!.index = index,
-          itemCount: 3,
-          itemBuilder: (_, int index) => Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: Material(
-                child: Ink.image(
-                  image: AssetImage(AppImages.banners[index]),
-                  fit: BoxFit.fitHeight,
-                  // child: InkWell(
-                  //   onTap: () {},
-                  //   customBorder: RoundedRectangleBorder(
-                  //     borderRadius: BorderRadius.circular(15),
-                  //   ),
-                  // ),
-                  // ! think about adding callback on banner
+    return Center(
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxHeight: 300),
+        child: AspectRatio(
+          aspectRatio: 2.13 / 1,
+          child: PageView.builder(
+            onPageChanged: (index) => bannerController!.index = index,
+            itemCount: 3,
+            itemBuilder: (_, int index) => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Material(
+                  child: Ink.image(
+                    image: AssetImage(AppImages.banners[index]),
+                    fit: BoxFit.fitHeight,
+                    // child: InkWell(
+                    //   onTap: () {},
+                    //   customBorder: RoundedRectangleBorder(
+                    //     borderRadius: BorderRadius.circular(15),
+                    //   ),
+                    // ),
+                    // ! think about adding callback on banner
+                  ),
                 ),
               ),
             ),

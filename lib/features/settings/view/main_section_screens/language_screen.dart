cbcca40f3ed0e287944148d5d90550f21b9fc15c
kobetskiy <kobetskiy22@gmail.com>
2024-06-07 12:09:39 +0300

    added language screen

--- lib/features/settings/view/main_section_screens/language_screen.dart
+++ lib/features/settings/view/main_section_screens/language_screen.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:traktor_family_gastro_bar/features/widgets/app_bar_widget.dart';
+import 'package:traktor_family_gastro_bar/generated/l10n.dart';
 
-class LanguageScreen extends StatelessWidget {
+class LanguageScreen extends StatefulWidget {
   const LanguageScreen({super.key});
 
   @override
+  State<LanguageScreen> createState() => _LanguageScreenState();
+}
+
+enum LanguageValues { en, uk, auto }
+
+class _LanguageScreenState extends State<LanguageScreen> {
+  LanguageValues? _currentValue = LanguageValues.uk;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: NestedScrollView(
+          floatHeaderSlivers: true,
+          headerSliverBuilder: (context, innerBoxIsScrolled) => [
+            AppBarWidget(title: S.of(context).language),
+          ],
+          body: Column(
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+              RadioListTile<LanguageValues>(
+                title: const Text('Українська'),
+                value: LanguageValues.uk,
+                groupValue: _currentValue,
+                onChanged: (LanguageValues? value) {
+                  _currentValue = value;
+                  setState(() {});
+                },
+              ),
+              RadioListTile<LanguageValues>(
+                title: const Text('English'),
+                value: LanguageValues.en,
+                groupValue: _currentValue,
+                onChanged: (LanguageValues? value) {
+                  _currentValue = value;
+                  setState(() {});
+                },
+              ),
+              RadioListTile<LanguageValues>(
+                title: const Text('Авто.'),
+                value: LanguageValues.auto,
+                groupValue: _currentValue,
+                onChanged: (LanguageValues? value) {
+                  _currentValue = value;
+                  setState(() {});
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
